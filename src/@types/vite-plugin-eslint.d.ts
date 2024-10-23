7473403a4a03fd1d91df35ec91938287fc93508d
gyanchand yadav <gyanchandrayadav556@gmail.com>
2024-10-23 17:35:55 +0530

    s

--- src/@types/vite-plugin-eslint.d.ts
+++ src/@types/vite-plugin-eslint.d.ts
@@ -1,5 +1,20 @@
 declare module 'vite-plugin-eslint' {
     import { Plugin } from 'vite';
-    export default function eslint(): Plugin;
-  }
-  
+
+    interface ViteEslintOptions {
+        eslintPath?: string;
+        lintOnStart?: boolean;
+        include?: string[];
+        exclude?: string[];
+        formatter?: string | ((results: any) => string);
+        emitWarning?: boolean;
+        emitError?: boolean;
+        failOnWarning?: boolean;
+        failOnError?: boolean;
+        errorOnUnmatchedPattern?: boolean;
+    }
+
+    function eslint(options?: ViteEslintOptions): Plugin;
+
+    export default eslint;
+}
